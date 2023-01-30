177e9f022225c32b06787d3a5ebc885ef0c5fa4a
Gagan Manjunatha Gowda <gagan.manjunatha_gowda@dfki.de>
2023-01-30 18:59:59 +0530

    Fixed Permission Error

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -9,6 +9,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 // export a default function for API route to work
 export default async function asynchandler(req, res) {
+  console.log('Token: ', process.env.GRAPHCMS_TOKEN);
   const graphQLClient = new GraphQLClient((graphqlAPI), {
     headers: {
       authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
@@ -20,13 +21,16 @@ export default async function asynchandler(req, res) {
       createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
     }
   `;
-
-  const result = await graphQLClient.request(query, {
-    name: req.body.name,
-    email: req.body.email,
-    comment: req.body.comment,
-    slug: req.body.slug,
-  });
-
-  return res.status(200).send(result);
+  try {
+    const result = await graphQLClient.request(query, {
+      name: req.body.name,
+      email: req.body.email,
+      comment: req.body.comment,
+      slug: req.body.slug,
+    });
+    return res.status(200).send(result);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send('Error creating comment: ', err.message);
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home({ posts }) {
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
-            <img src="https://i.ibb.co/zRPLVgB/About-us.jpg" />
             <PostWidget />
             <Categories />
           </div>
